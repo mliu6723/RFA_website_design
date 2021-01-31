84cec100fd67678de2a152da14c6073eeeb9045f
phoebechen2005 <74422531+phoebechen2005@users.noreply.github.com>
2021-01-31 13:33:27 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row} from 'react-bootstrap';
 import RFANavbar from "./components/RFANavbar";
 // import RFACard from "./components/RFACard";
-import InternPos from "./components/Volunteer/Internpos";
+import InternPos from "./components/Volunteer/InternPos";
 export default class App extends React.Component{
 
   render() {
